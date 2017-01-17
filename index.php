867113b3f36ab6f2bb93ef3d74b918cd7b373a01
rimosdev <rimos.aries@gmail.com>
2017-01-16 19:29:48 -0500

    optimizado el servicio RESTful a traves de metodo post

--- index.php
+++ index.php
@@ -57,25 +57,61 @@ $app->get('/detail/:id', function ($idEmp) use ($app){
 
     $app->render('employees_detail', compact('config','empleado'));
 });
-$app->run();
-
 //function for RESTFUL
-$app->post('/api', function () use ($app) {
+$app->post('/api/', function () use ($app) {
 
   	//validating if is POST
   	$min_salary = $app->request->post('min_salary');
   	$max_salary = $app->request->post('max_salary');
     $listEmp = json_decode(file_get_contents('employees.json'));
-  	if(trim($q) != '')
+    $ret = array();
+
+$xml = new SimpleXMLElement('<root/>');
+
+  	if(isset($min_salary) and isset($max_salary) and intval($min_salary) >=0 and intval($max_salary)>=0)
   	{
 	    //filtering for email
 	    $ret = array();
 	    foreach ($listEmp as $key => $empleado) {
-	    	if($empleado->salary >=$min_salary and $empleado->salary<=$max_salary)
-	    		$ret[] = $empleado;
+		    //get sallary in number format
+		    $sallary = preg_replace('/[^\d,\.]/', '', $empleado->salary);
+		    $sallary = preg_replace('/,(\d{2})$/', '.$1', $sallary);
+		    $sallary = str_replace(',', '', $sallary);
+	    	if($sallary >=$min_salary and $sallary<=$max_salary)
+	    	{
+	    		$add = array(
+	    						'id' => $empleado->id,
+	    						'name' => $empleado->name,
+	    						'email' => $empleado->email,
+	    						'salary' => $empleado->salary
+	    					);
+	    		$ret[] = $add;
+	    		array_walk_recursive($add, array ($xml, 'addChild'));
+	    	}
 	    }
-	    $listEmp = (object) $ret;
 	}
 
-    echo json_encode($listEmp);
+// creating object of SimpleXMLElement
+$xml_data = new SimpleXMLElement('<?xml version="1.0"?><data></data>');
+   array_to_xml($ret,$xml_data);
+
+print $xml_data->asXML();
+    // echo $xml;
 });
+
+function array_to_xml( $data, &$xml_data ) {
+    foreach( $data as $key => $value ) {
+        if( is_numeric($key) ){
+            $key = 'item'.$key; //dealing with <0/>..<n/> issues
+        }
+        if( is_array($value) ) {
+            $subnode = $xml_data->addChild($key);
+            array_to_xml($value, $subnode);
+        } else {
+            $xml_data->addChild("$key",htmlspecialchars("$value"));
+        }
+     }
+}
+
+
+$app->run();
